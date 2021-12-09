f21264613a978b792a9280f25a1503269a8dc8b8
Mücahit AKSAKAL <81565832+Mucahit-A@users.noreply.github.com>
2021-12-09 15:43:46 +0100

    Add files via upload

---
+++ section1-cohort10.sql
@@ -0,0 +1,17 @@
+SELECT name FROM tracks;
+--SELECT name FROM tracks;
+
+SELECT name,Composer FROM tracks;
+
+SELECT * FROM tracks;
+SELECT DISTINCT Composer FROM tracks;
+SELECT DISTINCT AlbumId,MediaTypeId FROM tracks;
+
+SELECT name 
+FROM tracks 
+WHERE Composer='Jimi Hendrix';
+
+SELECT *
+FROM invoices
+WHERE total>10
+
